c2a6305baa5c504ced82a3d7fb3f0b4f27191ed2
Alena Vasilchenko <vasilchenko.ale@gmail.com>
2019-01-28 15:10:48 +0300

    refactor: replace id's with classes in the middle block, small script refactoring

--- script.js
+++ script.js
@@ -1,49 +1,44 @@
 
-var last_btn = 'btn1';
-var last_block = 'block1';
+var last_btn = '.button-mob-1';
+var last_block = '.info-block-1';
 
-document.getElementById('block1').classList.add("info-block-visible");
-document.getElementById('btn1').classList.add("btn-on");
+document.querySelector('.info-block-1').classList.add("info-block-visible");
+document.querySelector('.button-mob-1').classList.add("btn-on");
 
-document.getElementById('btn1').onclick = button_click;
-document.getElementById('btn2').onclick = button_click;
-document.getElementById('btn3').onclick = button_click;
-document.getElementById('btn4').onclick = button_click;
+document.querySelector('.button-mob-1').onclick = button_click;
+document.querySelector('.button-mob-2').onclick = button_click;
+document.querySelector('.button-mob-3').onclick = button_click;
+document.querySelector('.button-mob-4').onclick = button_click;
 
 
 function  button_click(e){
 
 	e.target.classList.remove("basic");
 	e.target.classList.add("btn-on");
-	document.getElementById(last_block).classList.remove("info-block-visible");
-	document.getElementById(last_btn).classList.remove("btn-on");
-	document.getElementById(last_btn).classList.add("basic");
+	document.querySelector(last_block).classList.remove("info-block-visible");
+	document.querySelector(last_btn).classList.remove("btn-on");
+	document.querySelector(last_btn).classList.add("basic");
 
-	switch(e.target.id){
-		case 'btn1':
-			document.getElementById('block1').classList.add("info-block-visible");
-			last_btn = 'btn1';
-			last_block = 'block1';
-			break;
-		case 'btn2':
-			document.getElementById('block2').classList.add("info-block-visible");
-			last_btn = 'btn2';
-			last_block = 'block2';
-			break;
-		case 'btn3':
-			document.getElementById('block3').classList.add("info-block-visible");
-			last_btn = 'btn3';
-			last_block = 'block3';
-			break;
-		case 'btn4':
-			document.getElementById('block4').classList.add("info-block-visible");
-			last_btn = 'btn4';
-			last_block = 'block4';
-			break;
+	if(e.target.classList.contains('button-mob-1')) {
+		document.querySelector('.info-block-1').classList.add("info-block-visible");
+		last_btn = '.button-mob-1';
+		last_block = '.info-block-1';
+	} else if (e.target.classList.contains('button-mob-2')) {
+		document.querySelector('.info-block-2').classList.add("info-block-visible");
+		last_btn = '.button-mob-2';
+		last_block = '.info-block-2';
+	} else if (e.target.classList.contains('button-mob-3')) {
+		document.querySelector('.info-block-3').classList.add("info-block-visible");
+		last_btn = '.button-mob-3';
+		last_block = '.info-block-3';
+	} else if (e.target.classList.contains('button-mob-4')) {
+		document.querySelector('.info-block-4').classList.add("info-block-visible");
+		last_btn = '.button-mob-4';
+		last_block = '.info-block-4';
 	}
 }
 
-var img_arr = document.getElementsByClassName("blockimg");
+var img_arr = document.getElementsByClassName("block-image");
 var btn_arr = document.getElementsByClassName("block-button");
 var last_targ = null;
 
